26a123555b1db5ddd6bdea1a58ddda8261939f49
Javier Durán <javiduranj@gmail.com>
2022-04-26 17:29:05 +0200

    Subimos proyecto DAW

---
+++ play.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="icon" type="image/x-icon" href="img/favicon-32x32.png">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">  
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/fontawesome.min.js" integrity="sha512-5qbIAL4qJ/FSsWfIq5Pd0qbqoZpk5NcUVeAAREV2Li4EKzyJDEGlADHhHOSSCw0tHP7z3Q4hNHJXa81P92borQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Moment -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/locale/es.min.js"></script>
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Scripts -->
+    <script type="text/javascript" defer src="js/main.js"></script>
+    <script type="text/javascript" defer src="js/notices.js"></script>
+    <title>ChumPlay - Jugar</title>
+</head>
+
+<body>
+    <div class="sidebar">
+    <img src="img/logo.png" alt="ChumPlay" title="ChumPlay">
+    <a href="index.php"> <i class="fa fa-home fa-sm"></i>Noticias</a>
+        <a href="create_cup.php"><i class="fa fa-solid fa-trophy fa-sm"></i>Torneo</a>
+        <a class="active" href="play.php"><i class="fa fa-solid fa-futbol fa-sm"></i>Jugar</a>
+        <a href="rating.php"><i class="fa fa-solid fa-table-list fa-sm"></i>Clasificación</a>
+        <a href="rules.php"><i class="fa fa-solid fa-question fa-sm"></i>Reglas</a>
+        <a href="profile.php"><i class="fa fa-solid fa-user fa-sm"></i>Perfil</a>
+
+    </div>
+
+    <div class="content">
+        <h2>Jugar</h2>
+        <p>This example use media queries to transform the sidebar to a top navigation bar when the screen size is 700px or less.</p>
+        <p>We have also added a media query for screens that are 400px or less, which will vertically stack and center the navigation links.</p>
+        <h3>Resize the browser window to see the effect.</h3>
+        
+    </div>
+    <div class="footer">
+        Javier Durán - Desarrollo de Aplicaciones Web - Cesur
+    </div>
+</body>
+
+</html>
---
+++ rules.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="icon" type="image/x-icon" href="img/favicon-32x32.png">
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">  
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/fontawesome.min.js" integrity="sha512-5qbIAL4qJ/FSsWfIq5Pd0qbqoZpk5NcUVeAAREV2Li4EKzyJDEGlADHhHOSSCw0tHP7z3Q4hNHJXa81P92borQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Moment -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/locale/es.min.js"></script>
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Scripts -->
+    <script type="text/javascript" defer src="js/main.js"></script>
+    <script type="text/javascript" defer src="js/notices.js"></script>
+    <title>ChumPlay - Reglas</title>
+</head>
+
+<body>
+    <div class="sidebar">
+        <img src="img/logo.png" alt="ChumPlay" title="ChumPlay">
+        <a href="index.php"> <i class="fa fa-home fa-sm"></i>Noticias</a>
+        <a href="create_cup.php"><i class="fa fa-solid fa-trophy fa-sm"></i>Torneo</a>
+        <a href="play.php"><i class="fa fa-solid fa-futbol fa-sm"></i>Jugar</a>
+        <a href="rating.php"><i class="fa fa-solid fa-table-list fa-sm"></i>Clasificación</a>
+        <a class="active" href="rules.php"><i class="fa fa-solid fa-question fa-sm"></i>Reglas</a>
+        <a href="profile.php"><i class="fa fa-solid fa-user fa-sm"></i>Perfil</a>
+
+    </div>
+
+    <div class="content">
+        <h2>Reglas</h2>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce magna lectus, tristique et diam id, porta porta lacus. Nullam non erat maximus, ullamcorper ligula et, luctus arcu. Cras aliquam nec est nec egestas. Nullam tristique placerat ipsum eget tincidunt. Praesent quis enim at lacus dictum mattis. Nunc dictum mauris non sem sodales tincidunt. Ut quis tellus ipsum. Aliquam ut ultricies leo. Fusce quis metus lobortis, sollicitudin orci a, bibendum tortor. Fusce scelerisque feugiat eros. Nullam maximus nunc id massa sollicitudin, a fermentum augue semper. Suspendisse ullamcorper semper elit ut fringilla. Donec non elit vitae neque congue dictum sed ut nisi. Nullam in turpis facilisis, fringilla lorem eget, varius ipsum. Praesent tincidunt commodo sollicitudin.
+            </p>
+            Praesent eleifend efficitur dictum. Praesent rutrum in augue sit amet euismod. Duis ut nulla in mauris vestibulum facilisis. In auctor nisl ac lacus viverra, tincidunt commodo mauris ultrices. Nulla cursus diam mauris, et hendrerit purus imperdiet vitae. Aliquam hendrerit dolor ut tortor fringilla, quis condimentum nisl pharetra. Praesent ullamcorper sagittis ante. Aenean commodo ex augue, nec consequat odio convallis ut. Ut velit est, imperdiet sit amet dignissim sagittis, bibendum nec turpis.
+
+            Aliquam convallis, lectus eget viverra laoreet, neque enim posuere nibh, at hendrerit sapien arcu maximus lacus. Vestibulum id tincidunt nisl, quis mollis leo. Nam quis imperdiet orci, eget dapibus sem. Etiam pellentesque interdum fringilla. Pellentesque ac augue dapibus leo luctus ultrices. Donec pellentesque orci velit, at malesuada arcu eleifend vitae. Nulla facilisi. Donec eget sollicitudin nisl.
+            <p>
+            Nam pellentesque risus vel hendrerit consectetur. Donec lorem nulla, blandit ac tellus nec, commodo aliquet nunc. Suspendisse ut nibh rhoncus, tempor mauris et, efficitur arcu. Sed a convallis lorem. In vel tincidunt neque. Maecenas et consectetur nisi. Integer tempor ut neque non efficitur. Sed tincidunt iaculis volutpat. Maecenas in eleifend quam. Pellentesque vehicula accumsan pulvinar. Aenean quis vehicula justo. In egestas, odio vitae mollis porttitor, risus ligula laoreet turpis, et venenatis quam nulla nec lorem. Maecenas elementum libero vel massa tempus, quis elementum turpis maximus. Etiam non laoreet nulla, vitae euismod lacus. Ut euismod, nunc eu tristique tincidunt, odio ipsum pretium ipsum, eget consectetur ipsum metus et augue. In in mauris neque.
+
+            Nullam in dolor magna. Suspendisse rutrum dui sit amet neque imperdiet, non sodales orci iaculis. Mauris quis iaculis nulla. Vivamus et tincidunt ligula. Nulla aliquet arcu orci, eget semper sapien consequat a. Quisque est sem, tempor quis blandit in, tempus a ante. Nullam viverra porttitor pretium. Vivamus scelerisque quis nisl ornare auctor. Curabitur imperdiet orci congue sapien interdum, id mattis enim venenatis. Phasellus tincidunt nibh id diam luctus, nec facilisis magna condimentum.
+        </p>
+
+    </div>
+    <div class="footer">
+        Javier Durán - Desarrollo de Aplicaciones Web - Cesur
+    </div>
+</body>
+
+</html>
